f108bf9d5d6cb79cf61d9723baa0c018e22f3d91
Anders Pearson <anders@columbia.edu>
2016-12-03 21:05:28 +0000

    :bookmark: release 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="thraxilsettings",
-    version="0.6.2",
+    version="1.0.0",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/thraxil/thraxilsettings",
