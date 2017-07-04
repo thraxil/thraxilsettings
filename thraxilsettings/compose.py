39574fae459e104491bc813df6f4c56532258fbf
Anders Pearson <anders@thraxil.org>
2017-07-04 17:39:39 +0100

    add compose settings

---
+++ thraxilsettings/compose.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+# docker-compose settings doesn't really have to do much
+# since it's a dev environment and is mostly covered by
+# settings_shared. mostly, it just has to set up some
+# defaults
+import os
+import sys
+
+
+def common(**kwargs):
+    DEBUG = True
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+            'ATOMIC_REQUESTS': True,
+        }
+    }
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+    BROKER_URL = "amqp://guest:guest@rabbitmq:5672/"
+    return locals()
